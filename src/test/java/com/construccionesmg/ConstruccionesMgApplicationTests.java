8e34b142e9c267e3c3241f001cc5484578a343c9
Construcciones MG Developer <dev@construccionesmg.com>
2026-08-01 18:50:34 -0500

    Initial Spring Boot project setup with MongoDB and Thymeleaf dependencies

---
+++ src/test/java/com/construccionesmg/ConstruccionesMgApplicationTests.java
@@ -0,0 +1,12 @@
+package com.construccionesmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstruccionesMgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
